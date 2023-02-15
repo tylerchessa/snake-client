7edf36db296146b1fd62d153223f29b069184516
tylerchessa <tylerchessa@gmail.com>
2023-02-15 21:15:38 +0000

    connected play.js to client.js

--- play.js
+++ play.js
@@ -1,21 +1,22 @@
-const net = require("net");
+// const net = require("net");
+const connect = require("./client.js")
 
 // establishes a connection with the game server
-const connect = function() {
-  const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: '50541'
-  });
+// const connect = function() {
+//   const conn = net.createConnection({
+//     host: '165.227.47.243',
+//     port: '50541'
+//   });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
+//   // interpret incoming data as text
+//   conn.setEncoding("utf8");
 
-  conn.on('data', (dataInput) => {
-    console.log(dataInput);
-  })
-  
-  return conn;
-};
+//   conn.on('data', (dataInput) => {
+//     console.log(dataInput);
+//   })
+
+//   return conn;
+// };
 
 
 
