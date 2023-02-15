c703234e6a36f5e1532d3e6ccec57ca3f7e631bf
tylerchessa <tylerchessa@gmail.com>
2023-02-15 20:55:16 +0000

    received 'data' from server

---
+++ play.js
@@ -0,0 +1,23 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function() {
+  const conn = net.createConnection({
+    host: '165.227.47.243',
+    port: '50541'
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on('data', (dataInput) => {
+    console.log(dataInput);
+  })
+  
+  return conn;
+};
+
+
+
+console.log("Connecting ...");
+connect();
