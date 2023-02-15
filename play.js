46c9400619ab6409c9178932a986ade4aa20b2d4
tylerchessa <tylerchessa@gmail.com>
2023-02-15 23:20:43 +0000

    created input.js file

--- play.js
+++ play.js
@@ -1,23 +1,24 @@
 // const net = require("net");
 const connect = require("./client.js")
+const setupInput = require("./input.js")
 
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
+// const setupInput = function () {
+//   const stdin = process.stdin;
+//   stdin.setRawMode(true);
+//   stdin.setEncoding("utf8");
+//   stdin.resume();
+//   stdin.on("data", handleUserInput);
+//   return stdin;
+// };
 
-// stdin.on("data", handleUserInput);
+// // stdin.on("data", handleUserInput);
 
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
+// const handleUserInput = function (key) {
+//   if (key === '\u0003') {
+//     process.exit();
+//   }
+// };
 
 // establishes a connection with the game server
 // const connect = function() {
